442601e1bc54175a393d14f9fcc184510b795523
RafaelCss <rf2014souza@gmail.com>
2024-12-02 17:22:07 -0300

    feature - Criando controller

---
+++ GerenciadorEstoque.Api/Controllers/ProdutoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult BuscarProdutos([FromQuery] int id)
+        {
+            return Ok(new { id });
+        }
+    }
+}
