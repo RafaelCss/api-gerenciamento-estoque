05b0bd9320aadf40cf1c69e3cb339be45972751d
RafaelCss <rf2014souza@gmail.com>
2025-04-13 19:32:16 -0300

    feature/13-04-25 :  Criar classe campos personalizados

---
+++ Dominio/Entidades/CampoPersonalizado.cs
@@ -0,0 +1,11 @@
+﻿namespace Dominio.Entidades;
+
+public class CampoPersonalizado
+{
+    public int Id { get; set; }
+    public string NomeInterno { get; set; }     // "nomeVendedor"
+    public string Rotulo { get; set; }          // "Nome do Vendedor"
+    public string Tipo { get; set; }            // "string", "int", "bool", "date", etc.
+    public string NomeEntidade { get; set; }    // "Produto", "Cliente", etc.
+    public bool Obrigatorio { get; set; }
+}
---
+++ Dominio/Entidades/ValorCampoPersonalizado.cs
@@ -0,0 +1,10 @@
+﻿namespace Dominio.Entidades;
+
+public class ValorCampoPersonalizado
+{
+    public int CampoPersonalizadoId { get; set; }
+    public CampoPersonalizado Campo { get; set; }
+    public int EntidadeId { get; set; }         // Id do Produto, Cliente, etc.
+    public string NomeEntidade { get; set; }    // "Produto", "Cliente", etc.
+    public string Valor { get; set; }
+}
