1f32053309d4fe04251a1c0c8ec84c947245708e
RafaelCss <rf2014souza@gmail.com>
2025-04-14 21:12:06 -0300

    feature/14-04-25 : Criar campos personalizados

--- Dominio/Entidades/Base/Entidade.cs
+++ Dominio/Entidades/Base/Entidade.cs
@@ -1,12 +1,17 @@
 ﻿using Flunt.Notifications;
+using Flunt.Validations;
 
 namespace Dominio.Entidades.Base;
 
 public class Entidade : Notifiable<Notification>
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-
     public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
-
     public DateTime DataAtualizacao { get; set; } = DateTime.UtcNow;
+    public Contract<Entidade> Contrato()
+    {
+        var contract = new Contract<Entidade>();
+
+        return contract;
+    }
 }
--- Dominio/Entidades/CampoPersonalizado.cs
+++ Dominio/Entidades/CampoPersonalizado.cs
@@ -1,11 +1,39 @@
-﻿namespace Dominio.Entidades;
+﻿using Dominio.Entidades.Root;
+using Flunt.Validations;
 
-public class CampoPersonalizado
+namespace Dominio.Entidades;
+
+public class CampoPersonalizado :AggregateRoot
 {
-    public int Id { get; set; }
     public string NomeInterno { get; set; }     // "nomeVendedor"
     public string Rotulo { get; set; }          // "Nome do Vendedor"
     public string Tipo { get; set; }            // "string", "int", "bool", "date", etc.
     public string NomeEntidade { get; set; }    // "Produto", "Cliente", etc.
     public bool Obrigatorio { get; set; }
+
+    // Ef
+    public CampoPersonalizado() { }
+
+
+    public CampoPersonalizado(string nomeInterno , string rotulo , string tipo , string nomeEntidade , bool obrigatorio)
+    {
+        NomeInterno = nomeInterno;
+        Rotulo = rotulo;
+        Tipo = tipo;
+        NomeEntidade = nomeEntidade;
+        Obrigatorio = obrigatorio;
+    }
+
+    public CampoPersonalizado AdicionaNomeInterno(string nomeInterno)
+    {
+        NomeInterno = nomeInterno;
+
+        AddNotifications( Contrato()
+            .Requires()
+            .IsNotNullOrWhiteSpace(NomeInterno , nameof(NomeInterno) , "O nome interno não pode ser vazio.")
+        );
+
+        return this;
+    }
+
 }
