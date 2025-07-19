7401281ce1597531018d510821cadc1469468d48
RafaelCss <rf2014souza@gmail.com>
2025-07-18 23:09:52 -0300

    feat-18-07-25 : Ajustes gitignore

--- Dominio/Entidades/ValorCampoPersonalizado.cs
+++ Dominio/Entidades/ValorCampoPersonalizado.cs
@@ -4,7 +4,7 @@ public class ValorCampoPersonalizado
 {
     public int CampoPersonalizadoId { get; set; }
     public CampoPersonalizado Campo { get; set; }
-    public int EntidadeId { get; set; }         // Id do Produto, Cliente, etc.
-    public string NomeEntidade { get; set; }    // "Produto", "Cliente", etc.
-    public string Valor { get; set; }
+    public int EntidadeId { get; set; }      
+    public string NomeEntidade { get; set; }    
+    public decimal Valor { get; set; }
 }
